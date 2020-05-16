a738da0be23a6f852bcb112e7952c8a74cc32653
Ankit Sharad <Ankit@gmail.com>
2020-05-16 14:22:40 -0400

    Validating HTTP POST Request Body

--- mobile-app-ws/src/main/java/com/ankit/app/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/ankit/app/ui/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.ankit.app.ui.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ankit.app.ui.model.request.UserDetailsRequestModel;
 import com.ankit.app.ui.model.response.UserRest;
 
 @RestController
@@ -40,10 +45,17 @@ public class UserController {
 		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	
-	@PostMapping
-	public String createUser() 
+	@PostMapping( consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+	 			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) 
 	{
-		return "create user was called";
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+	
+		
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	
 	@PutMapping
